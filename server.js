4af89b89d7b48af8ffd470f13a18210c9cad1ceb
LIPL-232 <support@lemosys.com>
2026-02-24 18:21:29 +0530

    second commit

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ const server = http.createServer(app);
 // ─── Socket.io Setup ──────────────────────────────────────────────────────────
 const io = new Server(server, {
     cors: {
-        origin: 'https://taylor-frontend-phi.vercel.app' || '*',
-        methods: ['GET', 'POST'],
+        origin: 'https://taylor-frontend-phi.vercel.app',
         credentials: true,
     },
     pingTimeout: 60000,
@@ -40,7 +39,12 @@ chatHandler(io);
 app.set('io', io);
 
 // ─── Middleware ───────────────────────────────────────────────────────────────
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'https://taylor-frontend-phi.vercel.app',
+        credentials: true,
+    },
+));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
